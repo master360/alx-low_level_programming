2b0fa31551a16bb8715dda1e4ca6324f95b360e0
master360 <mtarig797@gmail.com>
2023-06-15 16:42:34 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
     /**
-     * main - Entry point
-     * Description:A C program that prints with put function
-     * Return: Always 0(Success)
+     * main - A program that prints a line with puts function 
+     
+     * Return: 0(Success)
     */
      * int main(void)
          {
-         printf("Programming is like building a multilingual puzzle\n");
-         return (0);
-         }
-
+         puts("\"Programming is like building a multilingual puzzle");
+         
+      return (0);
+	 }
