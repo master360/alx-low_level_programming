771d174e47b51efcdf405143ec81045e834dda84
master360 <mtarig797@gmail.com>
2023-06-16 11:07:23 -0700

    0x01-variables_if_else_while

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a string
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
