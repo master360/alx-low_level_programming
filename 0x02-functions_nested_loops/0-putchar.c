bc027eeab934b8f12431e128fbdc84317bda5550
master360 <mtarig797@gmail.com>
2023-06-20 13:07:05 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+ int main(void)
 {
-        char str[] = "_putchar";
-        int ch;
+    char str[] = "_putchar";
+    int ch;
 
-        for (ch = 0; ch < 8; ch++)
-                 _putchar(str[ch]);
-        _putchar('\n');
+      for (ch = 0; ch < 8; ch++)
+        _putchar(str[ch]);
+       _putchar('\n');
 
-        return (0);
+       return (0);
 }
