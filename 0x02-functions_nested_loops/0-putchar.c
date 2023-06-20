91c38071772dcf473e6a80ac956b610b2d88f6ee
master360 <mtarig797@gmail.com>
2023-06-20 10:17:35 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,23 @@
 /**
  * main -  Entry point
  *
- * Description: prints _putchar using putchar prototype
+ * Description: Prints "_putchar" using the _putchar prototype
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int mai(void)
+int main(void)
 {
-   char str[] = "_putchar";
+        char str[] = "_putchar";
         int ch;
-	for ( ch=0; ch > 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n'0;
-			return (0);
 
+        for (ch = 0; ch < 8; ch++)
+        {
+                _putchar(str[ch]);
+        }
 
+        _putchar('\n');
+
+        return (0);
 }
+
